8024f7be8efe285d47ce517710a99b844272914f
LiuHao0314 <74098342+LiuHao0314@users.noreply.github.com>
2020-11-19 20:44:31 +0800

    Updated Memory.java
    Change Samsung to GSkill.
    Add KLEVV.

--- src/Memory.java
+++ src/Memory.java
@@ -2,32 +2,50 @@ public class Memory {
     String name;
     String volume;
     double price;
-    public Memory(String n, String v, double p){
+
+    public Memory(String n, String v, double p) {
         name = n;
         volume = v;
         price = p;
     }
+
     public void print() {
-        System.out.println(name + " : volume is " + volume + ", and the price is " + price);
+        System.out.println(name + " : the volume is " + volume + ", and the price is " + price);
     }
 }
-class Samsung extends Memory {
-    public Samsung(String n, String v, double p) {
+
+class GSkill extends Memory {
+    public GSkill(String n, String v, double p) {
         super(n, v, p);
     }
-    static Samsung memory1 = new Samsung("Samsung-EDA", "16GB", 1028);
+
+    static GSkill memory1 = new GSkill("GSkill-TridentZ", "16GB", 799);
+
     public static void main(String[] args) {
         memory1.print();
     }
 }
+
 class Kingston extends Memory {
     public Kingston(String n, String v, double p) {
         super(n, v, p);
     }
+
     static Kingston memory2 = new Kingston("Kingston-boki", "16GB", 1328);
-    static Kingston memory3 = new Kingston("Kingston-KINGboki", "32GB", 3228);
+
     public static void main(String[] args) {
         memory2.print();
+    }
+}
+
+class KLEVV extends Memory {
+    public KLEVV(String n, String v, double p) {
+        super(n, v, p);
+    }
+
+    static KLEVV memory3 = new KLEVV("KLEVV-BoltX", "32GB", 759);
+
+    public static void main(String[] args) {
         memory3.print();
     }
 }
